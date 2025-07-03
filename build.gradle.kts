82f3332211f681144f95479e2ff08e78d47beff7
Woong99 <dndry1072@naver.com>
2025-07-03 22:26:23 +0900

    feat: Kotlin JPA Plugin 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,18 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.allopen") version "1.9.25"
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
+}
+
+apply(plugin = "kotlin-jpa")
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 group = "potatowonong"
