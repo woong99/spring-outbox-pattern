4e4b0fbcb391acf39acd8af950e79bb3616c668b
Woong99 <dndry1072@naver.com>
2025-07-03 20:40:56 +0900

    feat: mysql, kafka 기본 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,34 @@ repositories {
     mavenCentral()
 }
 
+configurations {
+    configureEach {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
+}
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Spring Data JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.2")
+
+    // Kotlin-Logging
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
+
+    // Kafka
+    implementation("org.springframework.kafka:spring-kafka")
+
+    // MySQL
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // Log4j2
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+
+    // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ src/main/kotlin/potatowonong/outboxpattern/global/config/kafka/KafkaConsumerConfig.kt
@@ -0,0 +1,33 @@
+package potatowonong.outboxpattern.global.config.kafka
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+
+@Configuration
+class KafkaConsumerConfig {
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, Any> {
+        val config = mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+            ConsumerConfig.GROUP_ID_CONFIG to "my-group",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+        )
+
+        return DefaultKafkaConsumerFactory(config)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = consumerFactory()
+
+        return factory
+    }
+}
